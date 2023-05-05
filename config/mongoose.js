b4e9b25c88060cb1e818a0c6518a3fd9b006983a
KenYuChang <justin0915justin0915@gmail.com>
2023-05-05 15:06:57 +0800

    done

---
+++ config/mongoose.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+const db = mongoose.connection;
+
+db.once("open", () => {
+  console.log("MongoDB connected!");
+});
+db.on("error", (err) => {
+  console.log(err);
+});
