338ea770de1b8c4748a6e62f5ce55c5207bdaff0
KenYuChang <justin0915justin0915@gmail.com>
2023-06-14 18:36:47 +0800

    fix:mongoose

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,5 +1,8 @@
 const mongoose = require('mongoose')
-mongoose.connect(process.env.ACOUNT_MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
 const db = mongoose.connection
 
 db.on('error', () => {
